a08de3f80492260dcd72577a5a7e0d3c0c018669
nguyen.h.a.dung <FRAMGIA\nguyen.h.a.dung@b120645-lt.framgia.com>
2018-04-04 15:09:20 +0700

    [Issue 01] Add config to auth with AJS

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ Item.destroy_all
   item_attributes = { name: Faker::Name.name, price: Faker::Number.number }
   Item.create! item_attributes
 end
+
+User.destroy_all
+User.create email: "user@gmail.com", name: "First User", password: "password123"
